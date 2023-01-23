720a558b5419e104bb7c2f8e09fdca76decce0a2
Marcin Szymczyk <szymczykm@outlook.com>
2023-01-23 11:11:13 +0100

    Getting trello api key and token from environment variables

--- TrelloApi/TrelloApiDriver.cs
+++ TrelloApi/TrelloApiDriver.cs
@@ -1,18 +1,29 @@
 ﻿using Microsoft.Playwright;
-using System.Text.Json;
 using TrelloApi.Models;
 
 namespace TrelloApi
 {
     internal class TrelloApiDriver
     {
-        const string TRELLO_API_KEY = "94dccbbfc9f2ff50c10b8066e2f01122";
-        const string TRELLO_TOKEN = "ATTAc9d224eddb552c5efadb500024fcdc15f5137bf2cbfe5c6d8aedf73bfa2236ab53DF829F";
-        const string TRELLO_AUTHORIZATION_PARAMS = $"key={TRELLO_API_KEY}&token={TRELLO_TOKEN}";
+        private readonly string? TRELLO_API_KEY;
+        private readonly string? TRELLO_TOKEN;
+        private readonly string TRELLO_AUTHORIZATION_PARAMS;
 
         public IAPIRequestContext RequestContext;
 
-        public TrelloApiDriver() => RequestContext = InitializeApiDriver().Result;
+        public TrelloApiDriver()
+        {
+            TRELLO_TOKEN = Environment.GetEnvironmentVariable("TRELLO_TOKEN");
+            TRELLO_API_KEY = Environment.GetEnvironmentVariable("TRELLO_API_KEY");
+
+            if(TRELLO_TOKEN is null || (TRELLO_API_KEY is null))
+            {
+                throw new NullReferenceException("TRELLO_TOKEN and TRELLO_API_KEY environment variables have to be set!");
+            }
+
+            TRELLO_AUTHORIZATION_PARAMS = $"key={TRELLO_API_KEY}&token={TRELLO_TOKEN}";
+            RequestContext = InitializeApiDriver().Result;
+        }
 
         public async Task<IAPIRequestContext> InitializeApiDriver()
         {
